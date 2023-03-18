13b1f55dac79529acf487871d83d7cb1c8da4f79
jyliao369 <jyliao369@gmail.com>
2023-03-18 19:08:18 -0400

    created a profile card

--- client/src/components/ProfileCard/index.js
+++ client/src/components/ProfileCard/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const ProfileCard = () => {
-  return <div>ProfileCard</div>;
+  const grabAccounts = () => {};
+
+  return (
+    <>
+      <div className="profileCard">
+        <div className="profileIcon"></div>
+        <p>First Name Last Name</p>
+        <p>username</p>
+        <p>email</p>
+        <div>
+          <button>Grab Accounts</button>
+        </div>
+      </div>
+      <div className="accountsCont"></div>
+    </>
+  );
 };
 
 export default ProfileCard;
